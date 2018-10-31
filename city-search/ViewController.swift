75cf9a05fee7d076022e1e531a248d0185289b86
James Langdon <corporatelangdon@gmail.com>
2018-10-31 12:24:46 -0400

    Initial commit

---
+++ city-search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  city-search
+//
+//  Created by James Langdon on 10/31/18.
+//  Copyright © 2018 corporatelangdon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
