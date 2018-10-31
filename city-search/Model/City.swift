1008921f2f126a29bea5de5d3f4d81330aba88a0
James Langdon <corporatelangdon@gmail.com>
2018-10-31 19:21:58 -0400

    Add City model

---
+++ city-search/Model/City.swift
@@ -0,0 +1,21 @@
+//
+//  City.swift
+//  city-search
+//
+//  Created by James Langdon on 10/31/18.
+//  Copyright © 2018 corporatelangdon. All rights reserved.
+//
+
+import Foundation
+
+struct Coordinate: Codable {
+    var lon: Double
+    var lat: Double
+}
+
+struct City: Codable {
+    var country: String
+    var name: String
+    var _id: Int
+    var coord: Coordinate
+}
