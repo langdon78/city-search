40392f61624044206be7014f7fc9c8dc3e295488
James Langdon <corporatelangdon@gmail.com>
2018-11-02 10:47:23 -0400

    Add CityRepository tests with progress stub

--- city-searchTests/UnitTests/CityRepositoryTests+ProgressStub.swift
+++ city-searchTests/UnitTests/CityRepositoryTests+ProgressStub.swift
@@ -12,25 +12,54 @@ import XCTest
 class CityRepositoryTests: XCTestCase {
     
     var cityRepository: CityRepository!
+    var fileLoadedCalled: Int = 0
+    var loadingDataTrieCalled: Int = 0
 
     override func setUp() {
-        cityRepository = CityRepository(data: mockCityData, delegate: nil)
+        cityRepository = CityRepository(data: mockCityData, delegate: self)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitializationProgress() {
+        XCTAssertEqual(fileLoadedCalled, 1)
+        XCTAssertEqual(loadingDataTrieCalled, 20) // Number of records in mock
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testInitalizationMalformedData() {
+        let cityRepositoryBad = CityRepository(data: mockCityDataMalformed, delegate: nil)
+        XCTAssertEqual(cityRepositoryBad?.count, 0)
+    }
+    
+    func testCityPrefixEmpty() {
+        cityRepository.executeFetchTask(with: "") { cities in
+            XCTAssertEqual(cities?.count, 20)
+        }
+    }
+    
+    func testCityPrefix() {
+        cityRepository.executeFetchTask(with: "ma") { cities in
+            XCTAssertEqual(cities?.map({$0.name}), ["Mahlsdorf","Markt Nordheim","Mariendorf"].sorted())
+        }
+    }
+    
+    func testCityPrefixNoMatch() {
+        cityRepository.executeFetchTask(with: "mars") { cities in
+            XCTAssertEqual(cities?.count, 0)
         }
     }
 
+}
+
+extension CityRepositoryTests: ProgressDelegate {
+    func fileLoaded() {
+        self.fileLoadedCalled += 1
+    }
+    
+    func loadingDataTrie(_ percent: Float) {
+        self.loadingDataTrieCalled += 1
+    }
+    
 }
